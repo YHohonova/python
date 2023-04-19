df60216ff46dea906e6b4697070033036b18c179
YHohonova <ygogonova91@gmail.com>
2023-04-19 21:43:44 +0200

    Homework #1

--- 6.py
+++
@@ -1,8 +0,0 @@
-first_day = int(input('Введите результат в км. в первый день тренировок:'))
-targ_res = int(input('Введите целевой результат в км.:'))
-day_num = 1
-while first_day < targ_res :
-    day_num += 1
-    first_day *= 1.1
-    print(first_day)
-print(f'Ответ: на{day_num}-й день спортсмен достиг результата- не менее {targ_res}км')
